fac7b2a40dbd6aff06193b104526f092cc650be5
Fredy Bambang <fredy@payfazz.com>
2021-11-19 00:14:54 +0700

    rebuild and update code

--- project/src/views/ForRecruiter.vue
+++ project/src/views/ForRecruiter.vue
@@ -7,23 +7,24 @@
         >
           <b>For Recruiter</b><br />
           <p>
-            I know that recruit experience tech talent is hype and high demand for this era.<br> 
+            I know that recruit experience <b>tech talent</b> is <u>hype</u> and <i>high demand</i> for this era.<br> 
             I'm still love work in my current company and it's like my comfort zone for my work life balance and i'm still learning a new stuff here.<br>
-            Personally i'm only a generalist backend programmer, that experienced create new project but not specialist in a single stack. 
+            Personally i'm only a <i>generalist backend programmer</i>, that experienced create new project but <u>not specialist in a single stack</u>. 
+            If you find a specialist, i think i'm not the person that you need.<br>
             Currently i use golang and postgres in my current company and follow the company coding style.<br><br>
 
             I accept side hustle that handle micro task or create a new microservice project or project consultation, and i can afford my time around 1 - 2 hours per day.
-            We can discuss about it as long it's backend side. If need frontend, i think i will ask my peer that usually using <b>React js</b> as their stack to help.<br>
-            And there the list what i personally can do to help: 
-            <ol>
+            We can discuss about it as long it's backend side. If need frontend, i think i will ask my peer that usually using <b>React js</b> as their stack to help.<br><br>
+            <b>And there the list what i personally can do to help:</b> 
+            <ul class="list-disc">
               <li>Define Project / Product requirement and the implementation like define table, relation</li>
               <li>Create CRUD api with Laravel (php), Golang or Adonis js (node js) as my backend engineering stack</li>
               <li>Dockerize golang / javascript project</li>
               <li>Deploy Laravel Project to shared hosting and setup domain</li>
               <li>For setup domain on vps, aws, gcp etc, maybe i can ask my peer to help or need do the research, because i never really done it myself</li>
               <li>Do common ci / cd using github action</li>
-              <li>Other ? just ask on my linked</li>
-            </ol>
+              <li>Other ? just ask on my <a href="https://www.linkedin.com/in/fredy-bambang-65094598/" class="underline text-blue-700 ...">linked</a></li>
+            </ul>
           </p>
         </div>
       </div>
